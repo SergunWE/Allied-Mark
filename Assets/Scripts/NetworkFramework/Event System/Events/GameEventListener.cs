1459974a18a88ce6412921c5d3000083db85b358
Sergun WE <sergunwe152@gmail.com>
2023-02-24 19:25:57 +0300

    Add event system

---
+++ Assets/Scripts/NetworkFramework/Event System/Events/GameEventListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+namespace NetworkFramework.EventSystem.Events
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [FormerlySerializedAs("Event")]
+        [Tooltip("Event to register with.")]
+        [SerializeField]
+        private GameEvent gameEvent;
+
+        [Tooltip("Response to invoke when Event is raised.")]
+        public UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+}
