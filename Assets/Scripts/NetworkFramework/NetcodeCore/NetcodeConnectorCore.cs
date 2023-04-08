c52e4c57ec9ca48457b54e62ac91ca3f9586d63b
Sergun WE <sergunwe152@gmail.com>
2023-04-08 13:45:15 +0300

    Added netcode base

---
+++ Assets/Scripts/NetworkFramework/NetcodeCore/NetcodeConnectorCore.cs
@@ -0,0 +1,15 @@
+namespace NetworkFramework.NetcodeCore
+{
+    public class NetcodeConnectorCore
+    {
+        public void StartHost()
+        {
+            
+        }
+
+        public void StartClient()
+        {
+            
+        }
+    }
+}
