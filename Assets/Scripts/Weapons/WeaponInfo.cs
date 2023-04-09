d1f46abfc8a8f55ef7523767359ed943bc75a1df
Sergun WE <sergunwe152@gmail.com>
2023-04-09 21:30:33 +0300

    Added weapon info

---
+++ Assets/Scripts/Weapons/WeaponInfo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon/Info")]
+public class WeaponInfo : ScriptableObject
+{
+    public string weaponName;
+    
+    public bool isAuto;
+    public bool isShotgun;
+    public bool haveSniperScope;
+
+    public int clipSize;
+}
